db2dbd2ec3bebbf7c54116447b7714f098bf0e27
Roman Hargrave <roman@hargrave.info>
2015-02-16 16:34:00 -0600

    Change student course count behaviour so that it returns the correct number of courses

--- src/models/models.c
+++ src/models/models.c
@@ -10,9 +10,165 @@
 #include <string.h>
 #include <stdio.h>
 #include "models.h"
+#include "../grading.h"
 
 const grade MAX_GRADE = 0xFF;
 const grade MIN_GRADE = 0x00;
+
+// Course --------------------------------------------------------------------------------------------------------------
+
+int Course_compareById(const void* a, const void* b){
+    if(!a | !b){
+        return (!a && !b) ? 0 : -1;
+    } else {
+        return ((Course*)a)->courseId - ((Course*)b)->courseId;
+    }
+}
+
+const char* Course_stringFormat = "Course{ .courseId = %03u, .courseName = \"%s\", .students[%02u] }";
+
+char* Course_toString(Course* course) {
+    if(!course) return "(Course)null";
+    char* nameFormatted[strlen(Course_stringFormat) + strlen(course->courseName)];
+    sprintf((char*) nameFormatted, Course_stringFormat, course->courseId, course->courseName, Course_studentsCount(course));
+
+    return nameFormatted;
+}
+
+size Course_studentsCount(Course* course) {
+    return PtrArray_CountSane((void**) course->students, NMEMBERS(course->students, Student*));
+}
+
+bool Course_addStudent(Course* course, Student* student) {
+
+    if(Course_studentsCount(course) >= NMEMBERS(course->students, Student*)) return false;
+
+    course->students[Course_studentsCount(course)] = student;
+
+    qsort(course->students, Course_studentsCount(course), sizeof(Student*), &Student_compareById);
+
+    return true;
+}
+
+bool Course_remStudentIndex(Course* course, size index) {
+
+    size initialSize = Course_studentsCount(course);
+
+    course->students[index] = NULL;
+
+    qsort(course->students, initialSize, sizeof(Student*), &Student_compareById);
+
+    return true;
+}
+
+bool Course_remStudent(Course* course, Student* student) {
+    size initialSize = Course_studentsCount(course);
+
+    for(size idx = 0; idx < initialSize; ++idx) {
+        if(Student_compareById(student, course->students[idx]) == 0){
+            return Course_remStudentIndex(course, idx);
+        }
+    }
+
+    return false;
+}
+
+// Student -------------------------------------------------------------------------------------------------------------
+
+int Student_compareById(const void *a, const void *b){
+    if(!a | !b) {
+      return (!a && !b) ? 0 : -1;
+    } else {
+        return ((Student*)a)->studentId - ((Student*)b)->studentId;
+    }
+}
+
+const char* Student_stringFormat = "Student{ .studentId = %03u, .studentName = \"%s\", .courses[%u] }";
+
+char* Student_toString(Student* student) {
+    if(!student) return "(Student)null";
+    char* nameFormatted[strlen(Student_stringFormat) + strlen(student->studentName) + 15];
+    sprintf((char*) nameFormatted, Student_stringFormat, student->studentId, student->studentName, Student_coursesCount(student));
+
+    return nameFormatted;
+}
+
+size Student_coursesCount(Student* student) {
+    // How to write hard-to-follow C code in one easy line:
+    for(size idx = 0; idx < NMEMBERS(student->courses, StudentEnrollment); ++idx) {
+        if(!student->courses[idx].course) return idx;
+    };
+
+    return 0;
+}
+
+long Student_courseIndex(Student* student, Course* course) {
+    size nCourses = Student_coursesCount(student);
+    for(size idx = 0; idx < nCourses; ++idx) {
+        if(Course_compareById(course, student->courses[idx].course) == 0) {
+            return idx;
+        }
+    }
+
+    return -1;
+}
+
+int StudentEnrollment_compareByCourseId(const void* a, const void* b) {
+    if(!a || !b) {
+        return (!a && !b) ? 0 : -1;
+    } else if(!((StudentEnrollment*)a)->course || !((StudentEnrollment*)b)->course) {
+        return (!((StudentEnrollment*)a)->course && !((StudentEnrollment*)b)->course) ? 0 : -1;
+    } else {
+        return ((StudentEnrollment*)a)->course->courseId - ((StudentEnrollment*)b)->course->courseId;
+    }
+}
+
+bool Student_removeCourse(Student* student, Course* course) {
+
+    long courseIdx = Student_courseIndex(student, course);
+    if(courseIdx < 0) return false;
+
+    size nCourses = Student_coursesCount(student);
+
+    student->courses[courseIdx].course = NULL;
+    student->courses[courseIdx].gradeCount = 0;
+    memset(student->courses[courseIdx].grades, 0, sizeof(student->courses[courseIdx].grades));
+
+    qsort(student->courses, NMEMBERS(student->courses, StudentEnrollment), sizeof(StudentEnrollment), &StudentEnrollment_compareByCourseId);
+
+    return true;
+}
+
+bool Student_addCourse(Student* student, Course* course) {
+
+    size nCourses = Student_coursesCount(student);
+    if(nCourses >= NMEMBERS(student->courses, Course*)) return false;
+
+    StudentEnrollment enrollment = {
+            .course = course
+    };
+
+    memset(enrollment.grades, 0, NMEMBERS(enrollment.grades, grade) * sizeof(grade));
+    enrollment.gradeCount = 0;
+
+    memcpy(&student->courses[nCourses], &enrollment, sizeof(StudentEnrollment));
+
+    return true;
+}
+
+bool Student_addGrade(Student* student, Course* course, grade grade) {
+
+    long courseIndex = Student_courseIndex(student, course);
+    if(courseIndex < 0) return false;
+
+    StudentEnrollment* courseRel = &student->courses[courseIndex];
+
+    GradeArray_addNew(courseRel->grades, NMEMBERS(courseRel->grades, grade), grade);
+    courseRel->gradeCount++;
+
+    return true;
+}
+
 
 // GradeBook -----------------------------------------------------------------------------------------------------------
 
@@ -80,115 +236,15 @@ size GradeBook_removeStudent(GradeBook* book, Student* student) {
 
 size GradeBook_removeStudentIndex(GradeBook* book, size index) {
     if(index < book->studentsCount) {
+        Student* original = &book->students[index];
+        size nCourses = Student_coursesCount(original);
+        for(size idx = 0; idx < nCourses; ++idx) {
+            Course_remStudent(original->courses[idx].course, original);
+        }
         Student* replacement = &book->students[--book->studentsCount];
         memmove(&book->students[index], replacement, sizeof(Student));
         memset(&book->students[book->studentsCount], 0, sizeof(Student));
         GradeBook_sortStudents(book);
     }
     return book->studentsCount;
-}
-
-// Course --------------------------------------------------------------------------------------------------------------
-
-int Course_compareById(const void* a, const void* b){
-    if(!a | !b){
-        return (!a && !b) ? 0 : -1;
-    } else {
-        return ((Course*)a)->courseId - ((Course*)b)->courseId;
-    }
-}
-
-const char* Course_stringFormat = "Course{ .courseId = %03u, .courseName = \"%s\", .students[%02u] }";
-
-char* Course_toString(Course* course) {
-    char* nameFormatted[strlen(Student_stringFormat) + strlen(course->courseName)];
-    sprintf((char*) nameFormatted, Course_stringFormat, course->courseId, course->courseName, Course_studentsCount(course));
-
-    return nameFormatted;
-}
-
-size Course_studentsCount(Course* course) {
-    return PtrArray_CountSane((void**) course->students, NMEMBERS(course->students, Student*));
-}
-
-// Student -------------------------------------------------------------------------------------------------------------
-
-int Student_compareById(const void *a, const void *b){
-    if(!a | !b) {
-      return (!a && !b) ? 0 : -1;
-    } else {
-        return ((Student*)a)->studentId - ((Student*)b)->studentId;
-    }
-}
-
-const char* Student_stringFormat = "Student{ .studentId = %03u, .studentName = \"%s\", .courses[%u] }";
-
-char* Student_toString(Student* student) {
-    char* nameFormatted[strlen(Student_stringFormat) + strlen(student->studentName) + 15];
-    sprintf((char*) nameFormatted, Student_stringFormat, student->studentId, student->studentName, Student_coursesCount(student));
-
-    return nameFormatted;
-}
-
-size Student_coursesCount(Student* student) {
-    size idx = 0;
-
-    // How to write hard-to-follow C code in one easy line:
-    while(idx < NMEMBERS(student->courses, StudentEnrollment) && student->courses[idx++].course);
-
-    return idx;
-}
-
-long Student_courseIndex(Student* student, Course* course) {
-    size nCourses = Student_coursesCount(student);
-    for(size idx = 0; idx < nCourses; ++idx) {
-        if(Course_compareById(course, student->courses[idx].course) == 0) {
-            return idx;
-        }
-    }
-
-    return -1;
-}
-
-int StudentEnrollment_compareByCourseId(const void* a, const void* b) {
-    if(!a || !b) {
-        return (!a && !b) ? 0 : -1;
-    } else if(!((StudentEnrollment*)a)->course || !((StudentEnrollment*)b)->course) {
-        return (!((StudentEnrollment*)a)->course && !((StudentEnrollment*)b)->course) ? 0 : -1;
-    } else {
-        return ((StudentEnrollment*)a)->course->courseId - ((StudentEnrollment*)b)->course->courseId;
-    }
-}
-
-bool Student_removeCourse(Student* student, Course* course) {
-
-    long courseIdx = Student_courseIndex(student, course);
-    if(courseIdx < 0) return false;
-
-    size nCourses = Student_coursesCount(student);
-
-    student->courses[courseIdx].course = NULL;
-    student->courses[courseIdx].gradeCount = 0;
-    memset(student->courses[courseIdx].grades, 0, sizeof(student->courses[courseIdx].grades));
-
-    qsort(student->courses, NMEMBERS(student->courses, StudentEnrollment), sizeof(StudentEnrollment), &StudentEnrollment_compareByCourseId);
-
-    return true;
-}
-
-bool Student_addCourse(Student* student, Course* course) {
-
-    size nCourses = Student_coursesCount(student);
-    if(nCourses >= NMEMBERS(student->courses, Course*)) return false;
-
-    StudentEnrollment enrollment = {
-            .course = course
-    };
-
-    memset(enrollment.grades, 0, NMEMBERS(enrollment.grades, grade) * sizeof(grade));
-    enrollment.gradeCount = 0;
-
-    memcpy(&student->courses[nCourses], &enrollment, sizeof(StudentEnrollment));
-
-    return true;
 }
