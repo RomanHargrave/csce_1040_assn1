a8dcc28379c2c221ad3273c453cc756319e90496
Roman Hargrave <roman@hargrave.info>
2015-02-18 19:46:40 -0600

    Add grade management command

--- src/shell/commands/enrollment.c
+++ src/shell/commands/enrollment.c
@@ -1,8 +1,10 @@
 #include <string.h>
 #include <strings.h>
+#include <stdint.h>
 #include "../../models/models.h"
 #include "command.h"
 #include "../../debug.h"
+#include "../../grading.h"
 
 static const char* ACTION_ADD = "add";
 static const char* ACTION_DEL = "rm";
@@ -83,5 +85,88 @@ ShellReturn Command_enroll(char* args, GradeBook* gradeBook) {
 
 ShellReturn Command_grade(char* args, GradeBook* gradeBook) {
 
-    return SR_FAILURE;
+    char* action    = strtok(args, " ");
+    char* sidString = strtok(NULL, " ");
+    char* cidString = strtok(NULL, " ");
+    char* numberStr = strtok(NULL, " ");
+
+    if(!action | !sidString | !cidString | !numberStr) {
+        d_printf("Args passed (in order) %s , %s , %s , %s\n", action, sidString, cidString, numberStr);
+        printf("You must specifiy an action (add, rm) a student ID, a course ID, and a grade or index\n");
+        return SR_FAILURE;
+    }
+
+    int sid             = atoi(sidString);
+    int cid             = atoi(cidString);
+    int gradeOrIndex    = atoi(numberStr);
+
+    if(!Student_isValidId(sid) || !Course_isValidId(cid)) {
+        printf("You must enter both a valid student ID, and a valid course ID\n");
+        return SR_FAILURE;
+    }
+
+    if(((gradeOrIndex < MIN_GRADE) || (gradeOrIndex > MAX_GRADE)) && (strcmp(action, ACTION_ADD) == 0)) {
+        printf("You must enter a grade between %u and %u\n", MIN_GRADE, MAX_GRADE);
+        return SR_FAILURE;
+    } else if(gradeOrIndex < 0) {
+        printf("You must enter an index greater than or equal to 0\n");
+        return SR_FAILURE;
+    }
+
+    Student* student    = Command_enroll_grade_findStudent(gradeBook, (byte) sid);
+    Course* course      = Command_enroll_grade_findCourse(gradeBook, (byte) cid);
+
+    if(!student) {
+        printf("No student with the id %i exists\n", sid);
+        return SR_FAILURE;
+    }
+
+    if(!course) {
+        printf("No course with the id %i exists\n", cid);
+        return SR_FAILURE;
+    }
+
+    long indexInStudent = Student_courseIndex(student, course);
+
+    if(indexInStudent < 0) {
+        printf("The student is not not enrolled in the specified course\n");
+        return SR_FAILURE;
+    }
+
+    StudentEnrollment* enrollment = &student->courses[indexInStudent];
+
+    if(!enrollment) {
+        printf("Invalid enrollment! Contact support with a coredump immediately.\n");
+        printf("idx=%l ptr=0x%x\n (%lu)\n", indexInStudent, (intptr_t) enrollment, (intptr_t) enrollment);
+        return SR_FAILURE;
+    }
+
+    if(strcmp(action, ACTION_ADD) == 0) {
+
+        Enrollment_addGrade(enrollment, (grade) gradeOrIndex);
+
+        printf( "Student grades in course updated\n"
+                "Average in course is now %f.\n", Enrollment_average(enrollment));
+
+    } else if(strcmp(action, ACTION_DEL) == 0) {
+
+        if(gradeOrIndex >= enrollment->gradeCount) {
+            printf("Speicifed grade index falls outside the grade count for this student\n");
+            return SR_FAILURE;
+        }
+
+        if(Enrollment_removeGrade(enrollment, (size) gradeOrIndex) == true) {
+
+            printf("The specified grade was removed from the student\n");
+
+        } else {
+
+            printf("Unable to remove grade (reason unknown)\n");
+
+            return SR_FAILURE;
+        }
+
+    }
+
+    return SR_SUCCESS;
 }
